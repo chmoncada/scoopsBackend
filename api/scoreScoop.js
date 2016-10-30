695cfbbb550dadf34672a35a573e71c1e3a9ae64
Charles Moncada <charles.moncada@emoshiapps.com>
2016-10-30 16:36:39 -0500

    Empezamos el API de calculo de valoracion

---
+++ api/scoreScoop.js
@@ -0,0 +1,28 @@
+/**
+ * Created by charlesmoncada on 30/10/16.
+ */
+
+//var azure = require("azure-mobile-apps");
+
+var api;
+api = {
+    get: function (request, response, next) {
+
+        var idScoop = request.query.idScoop;
+        var score = parseInt(request.query.score);
+        console.log('score: ' + score);
+        var query = "SELECT averageScore, personsScoring, title FROM scoops WHERE id = '" + idScoop + "'";
+
+        request.azureMobile.data.execute(query)
+            .then(function(results){
+                console.log("**** RESULTADOS DEL QUERY:" + results);
+                response.json(results)
+            });
+
+    }
+};
+
+api.get.access = 'anonymous';
+
+
+module.exports = api;
